d5f811e57475c8bb288b4f111d6adcc1f1950e97
J-hoplin1 <andrewyoon10@naver.com>
2021-05-15 06:46:22 +0000

    add Covid19 Bot With API

---
+++ Using Public API/CovidData.py
@@ -0,0 +1,100 @@
+import requests
+import json
+import re
+from pytz import timezone
+from typing import Any, MutableSequence
+from urllib.parse import urlencode, quote_plus, unquote
+from urllib.request import Request, urlopen
+from datetime import datetime, timedelta
+from bs4 import BeautifulSoup
+from lxml import html, etree
+import xml.etree.ElementTree as et
+from xml.dom import minidom 
+
+class openDataAPICall(object):
+    def __init__(self):
+        self.apiKey = ''
+        self.apiURL = 'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson'
+    
+    def buildRequests(self) -> BeautifulSoup: 
+        # 코드 실행한 시점
+        executedPoint = datetime.now(timezone('Asia/Seoul'))
+        endDate = executedPoint + timedelta(days = 1)# 하루뒤의 시간을 의미한다.
+        executedPoint = executedPoint + timedelta(days = -1)
+        #시작범위
+        searchStart = executedPoint.strftime("%Y%m%d") # strftime으로 포맷을 맞추어준다."%Y%m%d" : YYYYMMDD형태로 출력
+        #끝범위
+        searchEnd = endDate.strftime("%Y%m%d") # 끝범위를 다음날로 해줘야 오늘 날짜에 대한 값만 나온다.
+
+        #Request Query를 만든다.
+        queryParameter = '?' + urlencode({
+            quote_plus('serviceKey') : self.apiKey,
+            quote_plus('pageNo') : 1,
+            quote_plus('numOfRows') : 10,
+            quote_plus('startCreateDt') : searchStart,
+            quote_plus('endCreateDt') : searchEnd
+        })
+        response = requests.get(self.apiURL + queryParameter) # 기본적으로 requests를 인코딩한 반환값은 Byte String이 나오게 된다.
+        responseXML = response.text
+        responseCode = response.status_code
+        if 200 <= responseCode < 300:
+            res = BeautifulSoup(responseXML, 'lxml-xml')
+            return self.reProcessXML(res)
+        else:
+            return False
+    def addMainNews(self) -> MutableSequence:
+        covidSite = "http://ncov.mohw.go.kr/index.jsp"
+        covidNotice = "http://ncov.mohw.go.kr"
+        html = urlopen(covidSite)
+        bs = BeautifulSoup(html, 'html.parser')
+        # Bug fix 2021 03 02 : 파싱 구역 지정을 조금 더 구체화하였습니다 -> 카드 뉴스로 인한 방해
+        bs = bs.find('div',{'class' : 'm_news'})
+        sounds = []
+        briefTasks = dict()
+        hotIssues = dict()
+        mainbrief = bs.findAll('a',{'href' : re.compile('\/tcmBoardView\.do\?contSeq=[0-9]*')})
+        for brf in mainbrief:
+            briefTasks[brf.text] = covidNotice + brf['href']
+        sounds.append(briefTasks)
+        hotIssue = bs.findAll('a',{'href' : re.compile('https\:\/\/www\.korea\.kr\/special\/policyFocusView\.do\?newsId\=[0-9A-Za-z]*')})
+        for u in hotIssue:
+            hotIssues[u.text] = u['href']
+        sounds.append(hotIssues)
+        return sounds
+    
+    def reProcessXML(self,BSXML : BeautifulSoup) -> None:
+        res = BSXML# lxml-xml 매우빠르고 유일하게 지원되는 XML파서이다.
+        item = res.findAll('item')
+        dayBefore = item[1]
+        today = item[0]
+        news = self.addMainNews()
+
+        # 브리핑 관련 데이터
+        briefings = news[0]
+        briefTopics = list(briefings.keys())
+        #주요 이슈관련 데이터
+        hotIssues = news[1]
+        issueTopics = list(hotIssues.keys())
+        
+        dataDictionary = {
+            'dataDate' : datetime.strptime(today.find('stateDt').text,"%Y%m%d").date().strftime("%Y-%m-%d"),
+            'data' : {
+                'totalDecidedPatient' : today.find('decideCnt').text,
+                'todayDecidedPatient' : str(int(today.find('decideCnt').text) - int(dayBefore.find('decideCnt').text)),
+                'clearedPatient' : today.find('clearCnt').text,
+                'totalDeath' : today.find('deathCnt').text,
+                'increasedDeath' : str(int(today.find('deathCnt').text) - int(dayBefore.find('deathCnt').text)),
+                'CumulatedConfirmPercentage' : today.find('accDefRate').text 
+            },
+            'briefing' : {},
+            'hotIssue' : {}
+        }
+        for i,o in enumerate(briefTopics, start = 1):
+            dataDictionary['briefing']['briefTopics{}'.format(i)] = [o , briefings[o]]
+        for i,o in enumerate(issueTopics, start = 1):
+            dataDictionary['hotIssue']['issueTopics{}'.format(i)] = [o , hotIssues[o]]
+        return dataDictionary
+
+if __name__=="__main__":
+    p = openDataAPICall()
+    print(p.buildRequests())
---
+++ Using Public API/covid19bot.py
@@ -0,0 +1,61 @@
+import discord
+import asyncio
+import os
+from discord.ext import commands
+import urllib
+from urllib.request import URLError, HTTPError, urlopen, Request
+from bs4 import BeautifulSoup
+from urllib.parse import quote
+import re # Regex for youtube link
+import warnings
+import requests
+import time
+from CovidData import openDataAPICall
+
+client = discord.Client() # Create Instance of Client. This Client is discord server's connection to Discord Room
+bottoken = ""
+getCovidData = openDataAPICall()
+
+@client.event # Use these decorator to register an event.
+async def on_ready(): # on_ready() event : when the bot has finised logging in and setting things up
+    await client.change_presence(status=discord.Status.online, activity=discord.Game("Type !help or !도움말 for help"))
+    print("New log in as {0.user}".format(client))
+
+@client.event
+async def on_message(message): # on_message() event : when the bot has recieved a message
+    #To user who sent message
+    # await message.author.send(msg)
+    print(message.content)
+    if message.author == client.user:
+        return
+    if message.content.startswith("!코로나"):
+        APIResult = getCovidData.buildRequests()
+        if not APIResult:
+            embed = discord.Embed(title="Covid-19 Virus Korea Status : 아직 업데이트 되지 않음", description="",color=0x5CD1E5)
+            embed.add_field(name="Data source : 공공데이터 포털(data.go.kr) API", value="API정보 : 공공데이터활용지원센터_보건복지부 코로나19 감염 현황", inline=False)
+            embed.add_field(name="금일 정보가 아직 업데이트 되지 않았습니다!", value="아직 최신정보를 불러오지 못하였습니다. 평균 업데이트 시간은 오전 10시에서 11시사이입니다.", inline=False)
+            embed.add_field(name="중앙방역대책본부 코로나감염증 홈페이지 접속하기",value="http://ncov.mohw.go.kr/index.jsp",inline=False)
+            embed.set_thumbnail(url="https://wikis.krsocsci.org/images/7/79/%EB%8C%80%ED%95%9C%EC%99%95%EA%B5%AD_%ED%83%9C%EA%B7%B9%EA%B8%B0.jpg")
+            embed.set_footer(text='Service provided by Hoplin.',icon_url='https://avatars2.githubusercontent.com/u/45956041?s=460&u=1caf3b112111cbd9849a2b95a88c3a8f3a15ecfa&v=4')
+            
+        else:
+            embed = discord.Embed(title="Covid-19 Virus Korea Status", description="",color=0x5CD1E5)
+            embed.add_field(name="Data source : 공공데이터 포털(data.go.kr) API", value="API정보 : 공공데이터활용지원센터_보건복지부 코로나19 감염 현황", inline=False)
+            embed.add_field(name="Latest data refred time",value= f"해당 자료는 {APIResult['dataDate'].split('-')[0]}년 {APIResult['dataDate'].split('-')[1]}월 {APIResult['dataDate'].split('-')[2]}일 자료입니다", inline=False)
+            embed.add_field(name="전체 확진환자", value=f"{APIResult['data']['totalDecidedPatient']}명",inline=True)
+            embed.add_field(name="신규 확진자", value=f"{APIResult['data']['todayDecidedPatient']}명",inline=True)
+            embed.add_field(name="완치환자(격리해제)", value=f"{APIResult['data']['clearedPatient']}명", inline=True)
+            embed.add_field(name="전체 사망자", value=f"{APIResult['data']['totalDeath']}명", inline=True)
+            embed.add_field(name="신규 사망자", value=f"{APIResult['data']['increasedDeath']}명", inline=True)
+            embed.add_field(name="누적확진률", value=f"{APIResult['data']['CumulatedConfirmPercentage']}", inline=True)
+            briefKeys = list(APIResult['briefing'].keys())
+            newsKeys = list(APIResult['hotIssue'].keys())
+            for i in range(len(briefKeys)):
+                embed.add_field(name=f"- 최신 브리핑 {i + 1} : {APIResult['briefing'][briefKeys[i]][0]}",value = f"Link : {APIResult['briefing'][briefKeys[i]][1]}",inline=False)
+            for i in range(len(newsKeys)):
+                embed.add_field(name=f"- 메인 뉴스 {i + 1} : {APIResult['hotIssue'][newsKeys[i]][0]}",value = f"Link : {APIResult['hotIssue'][newsKeys[i]][1]}",inline=False)
+            embed.set_thumbnail(url="https://wikis.krsocsci.org/images/7/79/%EB%8C%80%ED%95%9C%EC%99%95%EA%B5%AD_%ED%83%9C%EA%B7%B9%EA%B8%B0.jpg")
+            embed.set_footer(text='Service provided by Hoplin.',
+                         icon_url='https://avatars2.githubusercontent.com/u/45956041?s=460&u=1caf3b112111cbd9849a2b95a88c3a8f3a15ecfa&v=4')
+        await message.channel.send("Covid-19 Virus Korea Status", embed=embed)
+client.run(bottoken)
